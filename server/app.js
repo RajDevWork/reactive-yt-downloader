e74edd0feaa7f4767bfac1a809fa90187505c1ce
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-03-19 16:11:53 +0530

    Updated get path

--- server/app.js
+++ server/app.js
@@ -75,7 +75,7 @@ app.post("/api/download", (req, res) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 // React routing fix
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
